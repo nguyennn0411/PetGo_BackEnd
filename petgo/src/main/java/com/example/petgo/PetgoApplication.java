a2e66f3a50f61c6df3d88408f3cc69f6ca0a9420
nguyennn0411 <phucnguyenxt2004@gmail.com>
2026-04-13 14:19:37 +0700

    create new

---
+++ petgo/src/main/java/com/example/petgo/PetgoApplication.java
@@ -0,0 +1,13 @@
+package com.example.petgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetgoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetgoApplication.class, args);
+    }
+
+}
